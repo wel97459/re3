9a1229e7a646b83b7eb5b4de2461604be006b50c
Winston Lowe <wel97459@gmail.com>
2025-02-21 10:40:32 -0800

    updated gitignore and get switch build working.

--- cmake/nx/NXFunctions.cmake
+++ cmake/nx/NXFunctions.cmake
@@ -1,27 +1,56 @@
-if(NOT COMMAND nx_generate_nacp)
-    message(FATAL_ERROR "The `nx_generate_nacp` cmake command is not available. Please use an appropriate Nintendo Switch toolchain.")
-endif()
-
-if(NOT COMMAND nx_create_nro)
-    message(FATAL_ERROR "The `nx_create_nro` cmake command is not available. Please use an appropriate Nintendo Switch toolchain.")
-endif()
+list(APPEND CMAKE_MODULE_PATH "$ENV{DEVKITPRO}/cmake/")
+include(Switch)
 
 set(CMAKE_EXECUTABLE_SUFFIX ".elf")
+set(NX_ICON "${PROJECT_SOURCE_DIR}/res/images/logo_256.jpg")
 
-function(reVC_platform_target TARGET)
+function(nx_create_nacp TARGET NAME AUTHOR VERSION)
+    set(NACP_PATH ${PROJECT_SOURCE_DIR}/src/${TARGET}.nacp)
+
+    message(INFO " Running nx_create_nacp")
+
+    execute_process(
+        COMMAND ${NX_NACPTOOL_EXE} "--create" ${NAME} ${AUTHOR} ${VERSION} ${NACP_PATH}
+        WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/src
+        OUTPUT_VARIABLE OUTPUT
+        ERROR_VARIABLE ERROR
+        RESULTS_VARIABLE RESULTS
+    )
+
+endfunction()
+
+function(nx_create_nro TARGET ICON)
+    set(NRO_PATH ${PROJECT_SOURCE_DIR}/src/${TARGET}.nro)
+    set(ELF_PATH ${PROJECT_SOURCE_DIR}/src/${TARGET}${CMAKE_EXECUTABLE_SUFFIX})
+    set(NACP_PATH ${PROJECT_SOURCE_DIR}/src/${TARGET}.nacp)
+
+    message(INFO " Running nx_create_nro")
+
+    execute_process(
+        COMMAND ${NX_ELF2NRO_EXE} ${ELF_PATH} ${NRO_PATH} --nacp=${NACP_PATH} --icon=${ICON}
+        WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/src
+        OUTPUT_VARIABLE OUTPUT
+        ERROR_VARIABLE ERROR
+        RESULTS_VARIABLE RESULTS
+    )
+
+endfunction()
+
+function(re3_platform_target TARGET NAME VERSION ICON )
     cmake_parse_arguments(RPT "INSTALL" "" "" ${ARGN})
 
     get_target_property(TARGET_TYPE "${TARGET}" TYPE)
     if(TARGET_TYPE STREQUAL "EXECUTABLE")
-        nx_generate_nacp(${TARGET}.nacp
-            NAME "${TARGET}"
-            AUTHOR "${${PROJECT}_AUTHOR}"
-            VERSION "1.0.0-${GIT_SHA1}"
+        nx_create_nacp(
+            ${TARGET}
+            ${NAME}
+            "${${PROJECT}_AUTHOR}"
+            "${VERSION}"
         )
 
-        nx_create_nro(${TARGET}
-            NACP ${TARGET}.nacp
-            ICON "${PROJECT_SOURCE_DIR}/res/images/logo_256.jpg"
+        nx_create_nro(
+            ${TARGET}
+            ${ICON}
         )
 
         if(${PROJECT}_INSTALL AND RPT_INSTALL)
